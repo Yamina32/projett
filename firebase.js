378a2191ef48e66360155b7a5d618c83508188f2
ouchaou <yaminaouchaou20@gmail.com>
2023-09-29 12:08:43 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app'
+
+const app = initializeApp({
+  apiKey: "AIzaSyCOjkmLipyFvxM389jF4zi9HUaXIoUzves", 
+  authDomain: "esgi-a9c62.firebaseapp.com", 
+  projectId: "esgi-a9c62", 
+
+})
+
+export default app
